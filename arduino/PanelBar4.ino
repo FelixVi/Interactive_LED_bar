b6ba81337d93669cbd71ffd1b320a6d4bc4517c0
wannabeCitizen <doorsofskirpan@gmail.com>
2016-04-17 12:04:02 -0600

    basically works, but need a fatter processor

--- arduino/PanelBar4.ino
+++ arduino/PanelBar4.ino
@@ -4,10 +4,11 @@
     
     On PSoC it's SDA 0.5 and SCL 0.4
 */
-#define TWI_FREQ 100000L
+#define TWI_FREQ 35000L
 
-#include <Wire.h>
+#include "Pan4.h"
 #include <Adafruit_NeoPixel.h>
+#include <Wire.h>
 #ifdef __AVR__
   #include <avr/power.h>
 #endif
@@ -15,51 +16,10 @@
 //Whatever Pin we want to control the lights from
 #define PIN 6
 
-//Establish light strip
-
-// 4 panels
-// uint16_t grid[28][7];
-
-//Which ones aren't chained correctly
-bool flipped[4] = { false, false, true, false};
-
-//----> Look-up tables for IR sensor orders
-//### 3 panel board: 0xC, 0xE, 0xD
-//first
-//int panel12[8] = {4, 0, 6, 2, 5, 1, 7, 3};
-//second
-//int panel14[8] = {3, 6, 5, 7, 0, 4, 1, 2};
-//third
-//int panel13[8] = {3, 7, 1, 6, 0, 2, 5, 4};
-
-//### 4 panel board: 0x09, 0x08, 0x0A, 0x0F
-// int panel9[8] = {7, 1, 2, 3, 0, 6, 4, 5};
-// int panel9old[8] = {0, 0, 0, 0, 0, 0, 0};
-
-// int panel8[8] = {0, 3, 5, 2, 1, 4, 7, 6};
-// int panel8old[8] = {0, 0, 0, 0, 0, 0, 0};
-
-// int panel10[8] = {6, 2, 4, 1, 7, 3, 5, 0};
-// int panel10old[8] = {0, 0, 0, 0, 0, 0, 0};
-
-// int panel15[8] = {7, 4, 2, 0, 6, 3, 5, 1};
-// int panel15old[8] = {0, 0, 0, 0, 0, 0, 0};
-
-// int *all[4] = {panel9, panel8, panel10, panel15};
-// int *allOld[4] = {panel9old, panel8old, panel10old, panel15old};
-
-int myPanels[4] = {9, 8, 10, 15};
-
-
 
 //Who is in motion
 int triggered[28][7];
-
-int animationx[8] = {0, 0, 2, 2, 4, 4, 6, 6};
-int animationy[8] = {0, 4, 2, 6, 0, 4, 3, 6};
-int loc[2];
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(200, PIN, NEO_GRB + NEO_KHZ800);
-uint32_t color = strip.Color(13, 197, 127);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(80, PIN, NEO_GRB + NEO_KHZ400);
 //Used to store most recent and prior read values
 int regs[32] = {0, 0, 0, 0, 0, 0, 0, 0,
                       0, 0, 0, 0, 0, 0, 0, 0,
@@ -71,46 +31,24 @@ int oldRegs[32] = {0, 0, 0, 0, 0, 0, 0, 0,
                       0, 0, 0, 0, 0, 0, 0, 0};
 
 //Need to fill this in the right locations of the IR nodes
-int placementsx[32];
-int placementsy[32];
-int breathState = 0;
+int placementsx[32] = {0, 0, 2, 2, 4, 4, 6, 6,
+                      13, 13, 11, 11, 9, 9, 8, 8,
+                      14, 14, 16, 16, 18, 18, 20, 20,
+                      22, 22, 23, 23, 25, 25, 27, 27};
+                      
+int placementsy[32] = {0, 4, 2, 4, 0, 4, 3, 5,
+                      2, 5, 1, 4, 2, 5, 1, 4,
+                      1, 4, 2, 5, 1, 4, 2, 5,
+                      1, 4, 2, 5, 1, 4, 2, 5};
+float breathState = 0;
 int breathUp = true;
 
-uint32_t colorsp = strip.Color(0, 197, 0); 
 //whatever our standard breathe tends toward
-int rgb[3] = {55,72,62}
-
-void breathe() {
-
-  if (breathUp){
-    breathState += .05;
-  } else {
-    breathState -= .05;
-  }
-  
-  r = breathState*rgb[0]
-  g = breathState*rgb[1]
-  b = breathState*rgb[2]
-  
-  uint32_t colorBr = strip.Color(r, g, b);
-  for (int x=0; x<28; x++) {
-    for (int y=0; y<7; y++) {
-      pixel = lednobyxy[x][y];
-      strip.setPixelColor(pixel, colorBr);
-    }
-  } 
-  if (breathState == 1){
-    breathUp = false;
-  } else if (breathState == 0) {
-    breathUp = true;
-  }
-
-}
-
+int rgb[3] = {0, 200, 0};
 
 void setup() {
   Wire.begin();
-  Serial.begin(9600);
+  Serial.begin(57600);
   strip.begin();
   initializeTriggers();
   strip.show();
@@ -138,7 +76,6 @@ int checkAvg(int node) {
   return diff;
 }
 
-
 void loop() {
   //Set initial normal state of all LEDs for breathe pattern
   breathe();
@@ -146,12 +83,12 @@ void loop() {
    for (int p=0; p<32; p++) {
      readNodes(IRaddr[p], IRreg[p], p);
      //During testing, we could look for abs values
-     if (checkAvs(p) > 4) {
-        x = placementsx[p];
-        y = placementsy[p];
+     if (checkAvg(p) > 4) {
+        int x = placementsx[p];
+        int y = placementsy[p];
         triggered[x][y] = 1;
      }
-   }
+  }
   for (int col = 0; col < 28; col++){
     for (int row = 0; row < 7; row++) {
       if (triggered[col][row] > 0) {
@@ -159,14 +96,77 @@ void loop() {
       }
     }
   }
-
    strip.show();
+   delay(90);
    //Do we want some kind of delay?
-   delay(2000);
+}
+
+void breathe() {
+
+  if (breathUp){
+    breathState += .05;
+  } else {
+    breathState -= .05;
+  }
+  
+  int r = breathState*rgb[0];
+  int g = breathState*rgb[1];
+  int b = breathState*rgb[2];
+  
+  uint32_t colorBr = strip.Color(g, r, b);
+  for (int x=0; x<28; x++) {
+    for (int y=0; y<7; y++) {
+      uint16_t pixel = lednobyxy[x][y];
+      strip.setPixelColor(pixel, colorBr);
+    }
+  } 
+    if (breathState > 1.0){
+    breathUp = false;
+  } else if (breathState < 0.05) {
+    breathUp = true;
+  }
 
 }
 
+void readNodes(int x, int y, int reg) {
+   int read1;
+   int read2;
+   int final_reading;
+   
+   Wire.beginTransmission(x);
+   Wire.write(y);
+   Wire.endTransmission();
+//   
+//
+    Wire.requestFrom(x, 1);
+    if (1 <= Wire.available()) {
+        read1 = Wire.read();
+    }
+    
+    Wire.beginTransmission(x);
+    Wire.write(y + 1);
+    Wire.endTransmission();
 
+    Wire.requestFrom(x, 1);
+    if (1 <= Wire.available()) {
+        read2 = Wire.read();  
+    }
+  
+    read1 = read1 * 256;
+    final_reading = read1 + read2;
+    oldRegs[reg] = regs[reg];
+    
+    //Store value
+    regs[reg] = final_reading;
+    if (final_reading > 3000) {
+      Serial.print("Sensor: ");
+      Serial.print(reg);
+      Serial.print(" = ");
+      Serial.print(final_reading);
+      Serial.println();
+    }
+}
+//
 void setAnimation(int col, int row) {
    uint16_t pixel;
    long dir = random(4);
@@ -178,7 +178,7 @@ void setAnimation(int col, int row) {
     return; 
    } else {
       //Play with different animation colors or we could randomize
-      uint32_t colorsp = strip.Color(0, 197, 0); 
+      uint32_t colorsp = strip.Color(0, 255, 0); 
       int temp = triggered[col][row];
       temp++;
       triggered[col][row] = 0;
@@ -189,7 +189,7 @@ void setAnimation(int col, int row) {
             break;
           }
           triggered[col][row-1] = temp;
-          pixel = grid[col][row-1];
+          pixel = lednobyxy[col][row-1];
           strip.setPixelColor(pixel, colorsp);
           break;
 
@@ -199,8 +199,7 @@ void setAnimation(int col, int row) {
             break;
           }
           triggered[col][row+1] = temp;
-          pixel = grid[col][row+1];
-
+          pixel = lednobyxy[col][row+1];
           strip.setPixelColor(pixel, colorsp);
           break;
 
@@ -210,7 +209,7 @@ void setAnimation(int col, int row) {
             break;;
           }
           triggered[col-1][row] = temp;
-          pixel = grid[col-1][row];
+          pixel = lednobyxy[col-1][row];
           strip.setPixelColor(pixel, colorsp);
           break;
 
@@ -220,7 +219,7 @@ void setAnimation(int col, int row) {
             break;
           }
           triggered[col+1][row] = temp;
-          pixel = grid[col+1][row];
+          pixel = lednobyxy[col+1][row];
           strip.setPixelColor(pixel, colorsp);
           break;
       }
@@ -283,23 +282,23 @@ void setAnimation(int col, int row) {
 //     }
 //   }
 // }
-  
-void lightGrid() {
-  
-  for(int i=0; i<28; i++){
-        for(int j=0; j<7; j++){
-            if(i%2 == 0){
-                strip.setPixelColor(grid[i][j], strip.Color(127, 127, 200));
-                strip.show();
-                delay(200);
-            } else {
-                strip.setPixelColor(grid[i][j], strip.Color(127, 127, 200));
-                strip.show();
-                delay(200);
-            }
-        }
-    }
-}
+//  
+//void lightGrid() {
+//  
+//  for(int i=0; i<28; i++){
+//        for(int j=0; j<7; j++){
+//            if(i%2 == 0){
+//                strip.setPixelColor(lednobyxy[i][j], strip.Color(127, 127, 200));
+//                strip.show();
+//                delay(200);
+//            } else {
+//                strip.setPixelColor(lednobyxy[i][j], strip.Color(127, 127, 200));
+//                strip.show();
+//                delay(200);
+//            }
+//        }
+//    }
+//}
   
 // void setGrid(int g) {
 //   //Go through each panel
@@ -333,35 +332,4 @@ void lightGrid() {
 // }
 
 
-void readNodes(int x, int y, int reg) {
-   int read1;
-   int read2;
-   int final_reading;
 
-   Wire.beginTransmission(x);
-   Wire.write(y);
-   Wire.endTransmission();
-
-    Wire.requestFrom(x, 1);
-    if (1 <= Wire.available()) {
-        read1 = Wire.read();
-        
-    }
-    Wire.beginTransmission(x);
-    Wire.write(byteno + 1);
-    Wire.endTransmission();
-
-    Wire.requestFrom(x, 1);
-    if (1 <= Wire.available()) {
-        read2 = Wire.read();        
-    }
-    read1 = read1 * 256;
-    final_reading = read1 + read2;
-    oldRegs[reg] = regs[reg]
-    
-    //Store value
-    regs[reg] = final_reading;
-        
-    // }
-    Serial.println("------------------");
-}
